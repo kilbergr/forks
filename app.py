2c00ea7fe155e2b53d09a7d2fffb622cabeb4eb4
Liza Daly <ldaly@law.harvard.edu>
2022-11-16 14:56:09 -0500

    Distinguish how far down the tree we are

--- app.py
+++ app.py
@@ -8,11 +8,6 @@ from jinja2 import FileSystemLoader, Environment
 g = Github(os.environ["GITHUB_TOKEN"])
 repo_name = os.environ.get("GITHUB_REPOSITORY", "lizadaly/forks")
 
-
-
-
-
-
 def get_all_forks(repo: Repository, forks: list[Repository]) -> list[Repository]:
 
     for fork in repo.get_forks():
@@ -20,19 +15,16 @@ def get_all_forks(repo: Repository, forks: list[Repository]) -> list[Repository]
         get_all_forks(fork, forks)
     return forks
 
+
 def main():
     repo = g.get_repo(repo_name)
-    root = repo if repo.parent is None else repo.parent  # FIXME find real parent
+    parent = repo.parent or repo
     forks = get_all_forks(repo, [])
 
     loader = FileSystemLoader(".")
     env = Environment(loader=loader)
     template = env.get_template("index.jinja")
-    Path('index.html').write_text(template.render({
-        "root": root,
-        "forks": forks
-    }))
-
+    Path("index.html").write_text(template.render({"repo": repo, "parent": parent, "forks": forks}))
 
 
 if __name__ == "__main__":
